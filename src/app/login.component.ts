ba5fe38e41fc158e39db653f6c76a02637b03380
VanessaAdjei <74023710+VanessaAdjei@users.noreply.github.com>
2023-12-16 01:43:17 +0000

    Add files via upload

---
+++ src/app/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.email, this.password)
+      .then((result: any) => {
+      
+      })
+      .catch((error: any) => {
+        
+        console.error('Login failed:', error);
+      });
+  }
+}
